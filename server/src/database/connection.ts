96610ec76837f3bcf8eaf36ddc1f4f26c54d09fc
lexvieira <lexvieira.info@gmail.com>
2021-02-16 00:45:24 +0700

    Migration and Seeds Using Knex, update routes

---
+++ server/src/database/connection.ts
@@ -0,0 +1,14 @@
+import knex from 'knex';
+
+const connection = knex({
+    client: 'mssql',
+    connection: {
+      host : '192.168.1.70',
+      user : 'SA',
+      password : 'JHu@hGTWSK@9t63',
+      database : 'MY_DB'
+    }
+});
+  
+export default connection;
+  
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const routes = express.Router()
 
 routes.get('/', (request, response) => {
-    response.json({ message: 'Hellooo world1' });
+    response.send('Invalid Route: Access the Correct Route <a href="http://localhost:3333">Here</a>');
 });
 
 export default routes;
