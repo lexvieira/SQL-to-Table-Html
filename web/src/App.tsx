c25128f09f5f2057328528f5bb00ddbc29d331f1
lexvieira <lexvieira.info@gmail.com>
2021-02-23 00:05:38 +0700

    Clean app, delete all code

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,27 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
-import Header from './Header';
-
 function App() {
-  const [counter, setCounter] = useState(0);
-
-
-  function handleButtonClick(){
-    setCounter(counter + 1); //immutability concept
-    console.log(counter);    
-  }
   return (
-         <div>
-           <Header title={`Counter: ${counter}`}/>
-
-           <h1>{counter}</h1>
-           <h1>{counter^2}</h1>
-
-           <button type="button" onClick={handleButtonClick}>Increase</button>                   
-           <h1>App Content</h1>
-
-         </div>
+        <h1>Hello World</h1>
   );
 }
 
