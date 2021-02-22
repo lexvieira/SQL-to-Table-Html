46f58cf8209b3970d45f9a8383bbacd65c213095
lexvieira <lexvieira.info@gmail.com>
2021-02-22 14:37:45 +0700

    Routes and Dev server conf package.json

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { response } from 'express';
+import knex from './database/connection';
 
-const routes = express.Router()
+import PointsController from "./controllers/PointsController";
+import  ItemsController  from "./controllers/ItemsController";
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController()
 
 routes.get('/', (request, response) => {
     response.send('Invalid Route: Access the Correct Route <a href="http://localhost:3333">Here</a>');
 });
+
+routes.get('/items', itemsController.index);
+routes.get('/points', pointsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
