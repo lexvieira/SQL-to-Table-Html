0111ddf949119e64474dcfa0cd49d66b3be071c9
lexvieira <lexvieira.info@gmail.com>
2021-03-10 19:51:30 +0700

    First Mobile Commit

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hellow World!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#FFF",
+    textDecorationLine: 'underline',
+  }
+});
